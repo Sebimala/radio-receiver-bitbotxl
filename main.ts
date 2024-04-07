908d1f38fee6eeb1cfbe2a24de43882eec4e0c31
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-07 15:24:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "acc") {
-        if (value > 5) {
-            bitbot.go(BBDirection.Forward, value)
-        } else if (value < -5) {
-            bitbot.go(BBDirection.Reverse, Math.abs(value))
+        if (value > 10) {
+            bitbot.go(BBDirection.Reverse, value)
+        } else if (value < -10) {
+            bitbot.go(BBDirection.Forward, Math.abs(value))
         } else {
             bitbot.stop(BBStopMode.Brake)
         }
     }
     if (name == "dir") {
-        if (value > 5) {
+        if (value > 15) {
             bitbot.rotate(BBRobotDirection.Left, value)
-        } else if (value < -5) {
+        } else if (value < -15) {
             bitbot.rotate(BBRobotDirection.Right, Math.abs(value))
         } else {
         	
