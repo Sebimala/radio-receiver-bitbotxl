2f3d43d59eb64314cf48a26b9bbbcd5c86a70fe7
Sebimala <86785937+Sebimala@users.noreply.github.com>
2024-04-07 15:30:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function updatescreen () {
+    basic.clearScreen()
+    led.plot(ledX, ledY)
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "acc") {
         if (value > 10) {
@@ -7,6 +11,7 @@ radio.onReceivedValue(function (name, value) {
         } else {
             bitbot.stop(BBStopMode.Brake)
         }
+        ledY = Math.round((value + 102) / 40)
     }
     if (name == "dir") {
         if (value > 15) {
@@ -16,7 +21,13 @@ radio.onReceivedValue(function (name, value) {
         } else {
         	
         }
+        ledX = Math.round((value + 102) / 40)
     }
+    updatescreen()
 })
+let ledX = 0
+let ledY = 0
 radio.setGroup(69)
 bitbot.select_model(BBModel.XL)
+ledY = 2
+ledX = 2
